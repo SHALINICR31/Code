518c560e1612714c2a0bd6adc0b5339a1c8dc6a7
CR SHALINI <mshaisha16@gmail.com>
2026-07-29 15:21:35 +0530

    Create MostContainWater.java

---
+++ TWO_POINTERS/MostContainWater.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int maxArea = 0;
+
+        while (left < right) {
+           
+            int width = right - left;
+       
+            int shortHeight = Math.min(height[left], height[right]);
+         
+            int currentArea = width * shortHeight;
+         
+            maxArea = Math.max(maxArea, currentArea);
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return maxArea;
+    }
+}
