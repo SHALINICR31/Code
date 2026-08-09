9f90eefb9f73c38c9e2b051d6fc2030ff906c2ac
CR SHALINI <mshaisha16@gmail.com>
2026-08-09 11:54:54 +0530

    Refactor reverseArray method to use array length

--- BASIC/ReverseArr.java
+++ BASIC/ReverseArr.java
@@ -1,7 +1,9 @@
-void reverseArray(int arr[], int n) {
-    int start = 0, end = n - 1;
+public void reverseArray(int[] arr) {
+    int start = 0, end = arr.length - 1;
     while (start < end) {
-        swap(arr[start], arr[end]);
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
         start++;
         end--;
     }
