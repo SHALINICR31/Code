7b7e56b598e1a2d5e54cf3f17e5a6f3daed11bc4
CR SHALINI <mshaisha16@gmail.com>
2026-08-13 16:12:44 +0530

    Update LongCommSeq.java

--- IN/LongCommSeq.java
+++ IN/LongCommSeq.java
@@ -5,25 +5,25 @@ public class EasyLCS {
         int m = s1.length();
         int n = s2.length();
         
-        // 1. எளிமையாக ஒரு 2D Matrix (டேபிள்) உருவாக்குகிறோம்
+        
         int[][] dp = new int[m + 1][n + 1];
 
-        // 2. இரண்டு லூப்களைப் பயன்படுத்தி டேபிளை நிரப்புகிறோம்
+
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
                 
-                // எழுத்துக்கள் ஒன்றாக இருந்தால் (Match)
+              
                 if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
                     dp[i][j] = 1 + dp[i - 1][j - 1]; // Diagonal + 1
                 } 
-                // எழுத்துக்கள் வேறாக இருந்தால் (Mismatch)
+                
                 else {
                     dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]); // Top vs Left (Max value)
                 }
             }
         }
         
-        // 3. டேபிளின் கடைசி மூலையில் இருக்கும் இறுதி விடையை அனுப்புகிறோம்
+
         return dp[m][n];
     }
 
