264f1cb1286a234b4b1453dd39d7b6f732cd9206
CR SHALINI <mshaisha16@gmail.com>
2026-08-13 16:04:59 +0530

    Update Binary_search.java

--- BINARY_SEARCH/Binary_search.java
+++ BINARY_SEARCH/Binary_search.java
@@ -1,1 +1,35 @@
+public class BinarySearchExample {
+    public static int binarySearch(int[] arr, int target) {
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low <= high) {
+            // Prevents potential integer overflow bug compared to (low + high) / 2
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == target) {
+                return mid; 
+            }
+            if (arr[mid] < target) {
+                low = mid + 1; 
+            } else {
+                high = mid - 1; 
+            }
+        }
+        return -1; 
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {2, 5, 8, 12, 16, 23, 38, 56, 72, 91};
+        int target = 23;
+        
+        int result = binarySearch(numbers, target);
+        
+        if (result != -1) {
+            System.out.println("Element found at index: " + result);
+        } else {
+            System.out.println("Element not found");
+        }
+    }
+}
 
