3f9fbf21fed3c882b4e764661e4a6617697391b2
CR SHALINI <mshaisha16@gmail.com>
2026-08-19 12:05:03 +0530

    Update Pow.java

--- GFG/Pow.java
+++ GFG/Pow.java
@@ -1,10 +1,16 @@
 class Solution {
     double power(double b, int e) {
-        // code here
-        if(e==0)
-        {
-            return 1;
+        if (e == 0) return 1.0; // Base case
+        
+        long exp = e;
+        if (exp < 0) {
+            b = 1.0 / b;
+            exp = -exp;
         }
-        return b* power(b,e-1);
+        
+        double halfPower = power(b, (int)(exp / 2));
+        
+        // Exponent Even ah iruntha square, Odd ah iruntha extra * b
+        return (exp % 2 == 0) ? halfPower * halfPower : b * halfPower * halfPower;
     }
 }
