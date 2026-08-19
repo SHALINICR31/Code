880f848a425f73cf91b16c43d866d95a8729a307
CR SHALINI <mshaisha16@gmail.com>
2026-08-19 16:05:06 +0530

    Create InsertAtEnd.java

---
+++ GFG/LinkedList/InsertAtEnd.java
@@ -0,0 +1,29 @@
+/*
+class Node{
+    int data;
+    Node next;
+
+    Node(int x){
+        data = x;
+        next = null;
+    }
+}
+*/
+class Solution {
+    public Node insertAtEnd(Node head, int x) {
+        // code here
+        Node newnode= new Node(x);
+        
+        if(head==null) { return newnode; }
+        
+        Node curr= head;
+        while(curr.next!=null)
+        {
+            curr=curr.next;
+        }
+        
+        curr.next=newnode;
+        
+        return head;
+    }
+}
